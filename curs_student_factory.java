2079990808b4485cb4ee4b8b9ef0456c11eeb3a1
Victor David <81818353+ClaysonG@users.noreply.github.com>
2022-05-22 16:05:46 +0300

    Delete curs_student_factory.java

--- curs_student_factory.java
+++
@@ -1,9 +0,0 @@
-import com.sun.istack.internal.Nullable;
-
-public abstract class curs_student_factory {
-    public abstract curs_student create_curs_student(String id_student, String id_curs,
-                                                     @Nullable int procent_examen, @Nullable int nota_examen,
-                                                     @Nullable int procent_laborator, @Nullable int nota_laborator,
-                                                     @Nullable int procent_seminar, @Nullable int nota_seminar,
-                                                     @Nullable int nota_proiect);
-}
